6286bcad59c33858492d5e15a2cba1533e87a5be
Jerry W <jerrywiltse@gmail.com>
2019-01-22 21:57:11 -0500

    use clang 5.0 for header only, add clang 7 to others, implement versions in recipes, conan no longer allows inheriting version, refresh travis badge, use .ci dir, python 3.6 in appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.67.0@bincrafters/testing")
 
 class BoostRegexConan(base.BoostBaseConan):
     name = "boost_regex"
+    version = "1.67.0"
     url = "https://github.com/bincrafters/conan-boost_regex"
     lib_short_names = ["regex"]
     options = {"shared": [True, False], "use_icu": [True, False]}
