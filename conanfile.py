b9411f2a2be2eb2376f304dff4e6bf76df4c6945
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:08 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,37 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostRegexConan(base.BoostBaseConan):
     name = "boost_regex"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_regex"
-    lib_short_names = ["regex"]
-    options = {
-        "shared": [True, False],
-        "use_icu": [True, False]
-    }
-    default_options = (
-        "shared=False", 
-        "use_icu=False"
-    )
-    b2_requires = [
-        "boost_assert",
-        "boost_concept_check",
-        "boost_config",
-        "boost_container_hash",
-        "boost_core",
-        "boost_integer",
-        "boost_iterator",
-        "boost_mpl",
-        "boost_predef",
-        "boost_smart_ptr",
-        "boost_static_assert",
-        "boost_throw_exception",
-        "boost_type_traits"
-    ]
-
-    def requirements_additional(self):
-        if self.options.use_icu:
-            self.requires("icu/63.1@bincrafters/stable")
+    version = "1.70.0"
